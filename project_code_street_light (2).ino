c4f601c55e6ebe961340ad86e5441137094d8b54
devang1497 <41756356+devang1497@users.noreply.github.com>
2018-07-27 01:05:54 +0530

    Add files via upload

---
+++ project_code_street_light (2).ino
@@ -0,0 +1,32 @@
+void setup() {
+
+  // put your setup code here, to run once:
+pinMode(8,INPUT);
+pinMode(3,OUTPUT);
+pinMode(4,OUTPUT);
+}
+
+
+void loop() {
+  
+// put your main code here, to run repeatedly:
+
+if(digitalRead(8)==0)
+
+{
+  digitalWrite(3,HIGH);
+  digitalWrite(4,LOW);
+
+  while(digitalRead(8)==0);
+}
+
+else if(digitalRead(8)==1)
+
+{
+  digitalWrite(3,LOW);
+  digitalWrite(4,HIGH);
+ 
+ while(digitalRead(8)==1);
+}
+
+}
